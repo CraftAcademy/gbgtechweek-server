a20e86e562cfb2591549652f315619a3c3ba4225
Amber <amber@amberwilkie.com>
2017-03-31 10:14:34 +0200

    Require name on contact form

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  it { is_expected.to have_db_column :id }
-  it { is_expected.to have_db_column :name }
-  it { is_expected.to have_db_column :email }
-  it { is_expected.to have_db_column :twitter }
-  it { is_expected.to have_db_column :company }
-  it { is_expected.to have_db_column :role }
-  it { is_expected.to have_db_column :location }
-  it { is_expected.to have_db_column :info }
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :twitter }
+    it { is_expected.to have_db_column :company }
+    it { is_expected.to have_db_column :role }
+    it { is_expected.to have_db_column :location }
+    it { is_expected.to have_db_column :info }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name}
+  end
+
 end
