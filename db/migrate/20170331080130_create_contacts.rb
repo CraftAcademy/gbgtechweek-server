132d9bbec5c37efb3c4f66525ddbbb86634e0dfe
Amber <amber@amberwilkie.com>
2017-03-31 10:13:17 +0200

    Add back in .rspec file

--- db/migrate/20170331080130_create_contacts.rb
+++ db/migrate/20170331080130_create_contacts.rb
@@ -1,6 +1,13 @@
 class CreateContacts < ActiveRecord::Migration[5.0]
   def change
     create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :twitter
+      t.string :company
+      t.string :role
+      t.string :location
+      t.string :info
 
       t.timestamps
     end
