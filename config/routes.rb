a2a219d1b62ecb48c2c845c0dbe5fb2a8c2208a7
tochman <thomas@craftacademy.se>
2017-10-29 11:32:49 +0100

    modifies routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :contacts, only: [:index, :new, :create]
+
+  root controller: :contacts, action: :new
+
   namespace :api do
     resources :contacts, only: [:index]
   end
-  root 'contacts#new'
+
+
 end
